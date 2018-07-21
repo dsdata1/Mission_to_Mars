36c10535483feebd413c7fcb9554dca4cc0c4f2c
dsdata1 <dsaw1121@gmail.com>
2018-07-21 01:05:46 -0700

    make it better

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mission_to_Mars
+
+In this Project Various websites are scraped to get the Mars Images and Data.  Using HTML, CSS, BootStrap, flask, as well as Mongo database, I recreated the webpage using the data scraped.
+
+In Order to Run this website:
+Please install:
+  1) mongo database
+  2) flask
+  3) And download the all the files to your computer
+  4) Run mongo database, and run app.py to start the flask
+  5) Using the localhost:5000, you can visualize the webpage on your computer
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_m
+import os
+
+
+is_prod = os.environ.get('MONGODB_URI', None)
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = is_prod
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars_mission=mars)
+
+#still need to work on it
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_m.scrape()
+    mars.update(
+        {},
+        mars_data,
+        upsert=True
+    )
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
