932fdef12b94ef76fa9362f75de53df83011ecb9
dsdata1 <28311765+dsdata1@users.noreply.github.com>
2018-07-21 23:20:10 -0700

    increase page timeout

--- scrape_m.py
+++ scrape_m.py
@@ -30,7 +30,7 @@ def scrape():
         driver.get("https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars/")
 
         #setting time out on page load
-        driver.set_page_load_timeout(5)
+        driver.set_page_load_timeout(10)
         # time.sleep(3)
         driver.execute_script('document.getElementById("full_image").click();')
         html = driver.page_source
